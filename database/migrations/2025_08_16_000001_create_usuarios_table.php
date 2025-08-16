e4f06a61683e367bc482ac61ea887ae332258529
ElCorbacho <histopat.cor@gmail.com>
2025-08-16 01:25:00 -0400

    migracion corregida

--- database/migrations/2025_08_16_000001_create_usuarios_table.php
+++ database/migrations/2025_08_16_000001_create_usuarios_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('correo')->unique();
+            $table->string('clave');
             $table->timestamps();
         });
     }
